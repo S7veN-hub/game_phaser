3e64700782553649cff2b245637b600e00ba152f
S7ven <yakabelmont@gmail.com>
2021-04-16 11:38:59 +0200

    Estructura basica proyecto

---
+++ js/charger.js
@@ -0,0 +1,10 @@
+var config = {
+    type: Phaser.AUTO,
+    width:800,
+    height: 600,
+	parent: "container",  			//contenedor del juego(id)
+	backgroundColor: "#000",
+    scene: []
+};
+
+var game = new Phaser.Game(config);
