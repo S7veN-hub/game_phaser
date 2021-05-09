d09203182dfd21a06ab19b7a873ef935a0b59ac4
S7ven <yakabelmont@gmail.com>
2021-05-09 12:43:46 +0200

    Level3 sin sistema de puntaje

--- js/scenes/Level3.js
+++ js/scenes/Level3.js
@@ -13,6 +13,8 @@ class Level3 extends Phaser.Scene {
 
         this.load.atlas("flying_aliens", "flying_aliens/flying_aliens.png", "flying_aliens/flying_aliens_atlas.json");
 
+        this.load.atlas("gold_coins", "gold_coins/gold_coins.png", "gold_coins/gold_coins_atlas.json");
+
         this.load.image("platform", "platform/platform.png");
         this.load.image("floor", "floor/floor.png");
     }
@@ -25,19 +27,43 @@ class Level3 extends Phaser.Scene {
         this.cont_flying_alien2 = 0;
         this.cont_flying_alien3 = 0;
         this.cont_platform = 0;
+        this.cont_coin = 0;
 
         this.floor = this.physics.add.image(this.width / 2, this.height / 2 + 300, "floor").setImmovable(true).setDepth(-1);
 
         this.heroe = this.physics.add.sprite(this.width / 2 - 100, this.height / 2, "dog").setScale(0.15);
         this.heroe.body.setGravityY(500);
         
+        this.events.on("load_coin", (h) => {
+            this.coin = this.physics.add.sprite(this.width + 100, h, "gold_coins").setDepth(-1);
+
+            this.tweens.add({
+                targets: this.coin,
+                x: -100,
+                duration: 10000
+            });
+
+            this.anims.create({       //gold coins animation
+                key: "gold",
+                frames: this.anims.generateFrameNames("gold_coins", {
+                    prefix: "gold_",
+                    start: 0,
+                    end: 7
+                }),
+                repeat: -1,
+                frameRate: 10
+            });
+            this.coin.anims.play("gold");
+
+            this.physics.add.collider(this.heroe, this.coin, this.coinCatched, null, this);
+        });
         this.events.on("load_platform", () => {
             this.platform = this.physics.add.image(this.width + 100, this.height / 2 + 10, "platform").setScale(0.2).setImmovable(true);
 
             this.tweens.add({
                 targets: this.platform,
                 x: -100,
-                duration: 5000,
+                duration: 5000
             });
 
             this.physics.add.collider(this.heroe, this.platform);
@@ -48,7 +74,7 @@ class Level3 extends Phaser.Scene {
             this.tweens.add({
                 targets: this.flying_alien1,
                 x: -100,
-                duration: 4000,
+                duration: 4000
             });
 
             this.anims.create({
@@ -71,7 +97,7 @@ class Level3 extends Phaser.Scene {
             this.tweens.add({
                 targets: this.flying_alien2,
                 x: -100,
-                duration: 6000,
+                duration: 6000
             });
 
             this.anims.create({
@@ -88,14 +114,13 @@ class Level3 extends Phaser.Scene {
 
             this.physics.add.collider(this.heroe, this.flying_alien2, this.alienCollision, null, this);
         });
-
         this.events.on("load_flying_alien3", (h) => {
             this.flying_alien3 = this.physics.add.sprite(this.width + 100, h, "flying_aliens").setScale(0.3);
 
             this.tweens.add({
                 targets: this.flying_alien3,
                 x: -100,
-                duration: 9000,
+                duration: 9000
             });
 
             this.anims.create({
@@ -140,6 +165,7 @@ class Level3 extends Phaser.Scene {
         this.cont_flying_alien2++;
         this.cont_flying_alien3++;
         this.cont_platform++;
+        this.cont_coin++;
         if (this.cont_platform == 180) {
             this.cont_platform = 0;
             this.events.emit("load_platform");
@@ -159,6 +185,11 @@ class Level3 extends Phaser.Scene {
             this.h = Phaser.Math.Between(20, this.height / 2);
             this.events.emit("load_flying_alien3", this.h);
         }
+        if (this.cont_coin == 100) {
+            this.cont_coin = 0;
+            this.h = Phaser.Math.Between(100, this.height / 2 + 200);
+            this.events.emit("load_coin", this.h);
+        }
 
         if (this.space.isDown && this.heroe.body.touching.down)  {
             this.heroe.setVelocityY(-400);
@@ -168,6 +199,9 @@ class Level3 extends Phaser.Scene {
     alienCollision() {
         console.log("Muerto :(");
         this.scene.pause("Level3");
+    }
+    coinCatched(heroe, coin) {
+        coin.destroy();
     }
 }
 
